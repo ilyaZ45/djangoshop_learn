69e17c7f2503db5acf8f69efb59d9f9740a4e5e3
ilyaZ45 <51793326+ilyaZ45@users.noreply.github.com>
2022-05-11 15:38:02 +0300

    price_base commit

--- crm/views.py
+++ crm/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from .models import Order
 from .forms import Orderform
+from price.models import PriceCard, PriceTable
+# from cms.models import CmsSlider
+
 
 # Create your views here.
 def first_page(request):
     object_list = Order.objects.all()
+    pc_1 = PriceCard.objects.get(pk=1)
+    pc_2 = PriceCard.objects.get(pk=2)
+    pc_3 = PriceCard.objects.get(pk=3)
+    price_table = PriceTable.objects.all()
     form = Orderform()
     return render(request, './index.html', {'object_list': object_list,
-                                            'form': form})
+                                            'form': form,
+                                            'pc_1': pc_1,
+                                            'pc_2': pc_2,
+                                            'pc_3': pc_3,
+                                            'price_table': price_table,
+                                            })
 
 def thanks(request):
     # name = request.GET['name']
